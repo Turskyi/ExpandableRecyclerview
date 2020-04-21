18888a2000162f6d41e28e8f9aa7e469329ad1d1
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-04-21 11:20:05 +0300

    update

--- app/src/main/java/ua/turskyi/expandedrecyclerview/MyApplication.kt
+++ app/src/main/java/ua/turskyi/expandedrecyclerview/MyApplication.kt
@@ -4,14 +4,13 @@ import android.app.Application
 import ua.turskyi.expandedrecyclerview.utils.Utils
 
 class MyApplication : Application() {
+    companion object {
+        var instance: MyApplication? = null
+            private set
+    }
     override fun onCreate() {
         super.onCreate()
         instance = this
         Utils.init(this)
-    }
-
-    companion object {
-        var instance: MyApplication? = null
-            private set
     }
 }
